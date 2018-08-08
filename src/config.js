dea0bbdce10f704e9213cd8ef93d35ab8b2fedf6
Jun Shao <mr.shaojun@gmail.com>
2018-08-08 23:15:38 +1000

    Prod and dev use different apiBase

--- src/config.js
+++ src/config.js
@@ -1,4 +1,26 @@
-export default {
+
+const basic = {
     'apiBase': 'http://localhost:5000/',
     'publicUrls': ['', '/', '/login', '/signup']
 }
+
+// For production environment
+const prod = {
+    'apiBase': '/',
+}
+
+// For development environment (usually localhost)
+const dev = {};
+
+let envConfig;
+switch(process.env.REACT_APP_RUNTIME_ENVIRONMENT) {
+    case 'development':
+        envConfig = dev;
+        break;
+    case 'production':
+    default:
+        envConfig = prod;
+}
+
+const config = Object.assign(basic, envConfig);
+export default config;
